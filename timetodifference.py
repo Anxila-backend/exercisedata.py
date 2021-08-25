59bd60f884b6609fb1904d83ce8c78a3cdb84793
Athirah <82761741+Athirah-ux@users.noreply.github.com>
2021-08-25 23:21:01 +0800

    Create timetodifference.py

---
+++ timetodifference.py
@@ -0,0 +1,23 @@
+void main (){
+  print(TimeToDifference(["1:10pm", "4:40am", "5:00pm" ]))
+}
+
+{
+String TimeDifference(List strArr) }
+  String TimeToDifference = strArr[0];
+  String dict = strArr [1]
+  List strArr parseDict = strArr[1].TimeDifference(',')
+
+  for(String word in parseDict) {
+    if (TimeToDifference.startWith)) {
+    
+    if (word.length == TimeToDifference.length){
+      return strArr;
+    }
+    return strArr + ',' + TimeDifference([TimeToDifference.substring(word.length), dict]);
+    }
+  }
+  // code goes here  
+  return strArr[0];
+
+}
